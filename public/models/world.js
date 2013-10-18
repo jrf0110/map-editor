dae51d0ea4c02bbed313377c476c22aab66e5a16
John Fawcett <jrf0110@gmail.com>
2013-10-18 10:59:34 -0500

    thing position caching and boundary detection

--- public/models/world.js
+++ public/models/world.js
@@ -7,7 +7,15 @@ define(function(require){
 
   , setTransforms: {
       things: function( val ){
-        return new Things( val, { world: this } );
+        var things = new Things( val, { world: this } );
+
+        this.thingPositions = {};
+
+        things.each( this.cacheThingPosition.bind( this ) );
+
+        things.on( 'change:x change:y', this.onThingsChange, this );
+
+        return things;
       }
     }
 
@@ -16,7 +24,33 @@ define(function(require){
       if ( x >= this.attributes.tiles.length ) return false;
       if ( y >= this.attributes.tiles.length ) return false;
 
+      if ( this.thingPositions[ x + 'x' + y ] ) return false;
+
       return true;
+    }
+
+  , cacheThingPosition: function( thing ){
+      var x, y, l, ll;
+
+      if ( thing.changedAttributes() ){
+        for ( y = thing.previous('y'), l = thing.previous('y') + thing.previous('height'); y < l; ++y ){
+          for ( x = thing.previous('x'), ll = thing.previous('x') + thing.previous('width'); x < ll; ++x ){
+            delete this.thingPositions[ x + 'x' + y ];
+          }
+        }
+      }
+    
+      for ( y = thing.get('y'), l = thing.get('y') + thing.get('height'); y < l; ++y ){
+        for ( x = thing.get('x'), ll = thing.get('x') + thing.get('width'); x < ll; ++x ){
+          this.thingPositions[ x + 'x' + y ] = true;
+        }
+      }
+
+      return this;
+    }
+
+  , onThingsChange: function( thing ){
+      this.cacheThingPosition( thing );
     }
   });
 
