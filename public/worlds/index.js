c2bc4e0d67685d06bba16b54dae62b84957bfffd
John Fawcett <jrf0110@gmail.com>
2013-11-06 19:56:28 -0600

    yet more re-organization

--- public/worlds/index.js
+++
@@ -1,8 +0,0 @@
-define(function(require){
-  var _ = require('underscore');
-
-  return _.extend( {}
-  , require('./grass')
-  , require('./something')
-  );
-});
--- routes/battle/battle-routes.js
+++ routes/battle/battle-routes.js
@@ -2,6 +2,35 @@ var db    = require('db');
 var m     = require('middleware');
 
 module.exports = function( app ){
+  app.get( '/api/battles'
+  , m.pagination()
+  , m.find( db.battles )
+  );
+
+  app.post('/api/battles'
+  , m.insert( db.battles )
+  );
+
+  app.get( '/api/battles/:id'
+  , m.param( 'id' )
+  , m.findOne( db.battles )
+  );
+
+  app.put( '/api/battles/:id'
+  , m.param( 'id' )
+  , m.update( db.battles )
+  );
+
+  app.patch( '/api/battles/:id'
+  , m.param( 'id' )
+  , m.update( db.battles )
+  );
+
+  app.del( '/api/battles/:id'
+  , m.param( 'id' )
+  , m.remove( db.battles )
+  );
+
   app.get('/battles/:id'
   , m.view( 'battle/battle-view.html' )
   );
